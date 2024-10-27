89b2cd892a33b8d2653ce7dba59acb14c38d0e9d
Rachel <156853570+naylehcar@users.noreply.github.com>
2024-10-27 03:00:03 -0400

    completed notification functionality ! ! removed popup features, + other minor updates

--- background.js
+++ background.js
@@ -1,26 +1,49 @@
-let storedPageSource = "";
+let thredUrl = "";
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "openPopup") {
-    console.log("background.js listener received popup message");
-    chrome.action.openPopup();
-  }
-});
-
-// Store the page source from content script
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "storePageSource") {
-    storedPageSource = message.source;
-  } 
-  else if (message.action === "getPageSource") {
-    sendResponse({ source: storedPageSource });
-  }
-});
+console.log("Background service worker is running.");
 
 
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ["content.js"]
-    });
+// Listens for a notification click & opens the thredUp page if so
+chrome.notifications.onClicked.addListener((notificationId) => {
+  console.log("Notification clicked:", notificationId);
+
+  chrome.storage.local.get("notificationId", (data) => {
+    if (data.notificationId === notificationId) {
+      console.log("TAB URL: " + thredUrl);
+      chrome.tabs.create({ url: thredUrl }, (tab) => {
+        if (chrome.runtime.lastError) {
+          console.error("Error creating tab:", chrome.runtime.lastError.message);
+        }
+      });
+      chrome.notifications.clear(notificationId);
+    }
   });
+});
+
+
+// Listens for the setUrl message from content.js & sets the thredUrl of background.js to the passed-in thredUp URL.
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.action === "setUrl") {
+    thredUrl = message.data.url;
+    console.log("url in listener: " + thredUrl);
+  }
+});
+
+
+// Listens for the createNotification message from content.js & creates a notification.
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.action == "createNotification") {
+    console.log("notification being created");
+    chrome.notifications.create({
+      type: "basic",
+      iconUrl: "icon.png",
+      title: "Product Recommendation Available",
+      message: "Click here to see similar items on thredUp!",
+      priority: 2,
+      requireInteraction: true,
+    }, (notificationId) => {
+      chrome.storage.local.set({ "notificationId": notificationId });
+    });
+  }
+  return true;
+});
